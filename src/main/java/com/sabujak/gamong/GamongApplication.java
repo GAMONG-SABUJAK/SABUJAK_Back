92d8dcb3eaa65d67632088971c4115d700af7c85
aitchhhan <aitchhan@gmil.com>
2025-11-28 13:51:00 +0900

    [add] remote ãrepository connection

---
+++ src/main/java/com/sabujak/gamong/GamongApplication.java
@@ -0,0 +1,13 @@
+package com.sabujak.gamong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabujak/gamong/GamongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabujak.gamong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
