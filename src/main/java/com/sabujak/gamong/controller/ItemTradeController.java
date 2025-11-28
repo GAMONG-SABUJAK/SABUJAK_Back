f4ae59b7de34f2d03bec30d81a8f3119462cbc06
aitchhhan <aitchhan@gmil.com>
2025-11-29 02:09:25 +0900

    [feat] uploadItemTrade

--- src/main/java/com/sabujak/gamong/controller/ItemTradeController.java
+++ src/main/java/com/sabujak/gamong/controller/ItemTradeController.java
@@ -1,4 +1,28 @@
 package com.sabujak.gamong.controller;
 
+import com.sabujak.gamong.domain.User;
+import com.sabujak.gamong.dto.Request.ReqItemTrade;
+import com.sabujak.gamong.service.ItemTradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/itemtrade")
 public class ItemTradeController {
+
+    private final ItemTradeService itemTradeService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<Void> uploadItemTrade(@AuthenticationPrincipal User user, ReqItemTrade reqItemTrade) {
+        itemTradeService.uplodeItemTrade(user, reqItemTrade);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
 }
