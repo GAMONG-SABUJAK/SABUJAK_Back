d367686b6cd49f756d181985263b0b1d19272bef
aitchhhan <aitchhan@gmil.com>
2025-11-28 17:32:01 +0900

    [add] empty ItemTradeReposiroty in repository

---
+++ src/main/java/com/sabujak/gamong/repository/ItemTradeRepository.java
@@ -0,0 +1,7 @@
+package com.sabujak.gamong.repository;
+
+import com.sabujak.gamong.domain.ItemTrade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemTradeRepository extends JpaRepository<ItemTrade, Long> {
+}
