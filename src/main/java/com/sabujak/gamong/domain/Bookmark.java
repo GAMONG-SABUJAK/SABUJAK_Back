a0c27a513e2a2b1483280c2d67cd2d5d410dedb7
aitchhhan <aitchhan@gmil.com>
2025-11-28 16:02:06 +0900

    [add] Bookmark domain

---
+++ src/main/java/com/sabujak/gamong/domain/Bookmark.java
@@ -0,0 +1,36 @@
+package com.sabujak.gamong.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Bookmark {
+    @Id
+    @GeneratedValue
+    private Long id; // pk
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user; // 즐겨찾기 한 유저
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_trade_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ItemTrade itemTrade; // 즐겨찾기 된 거래글
+
+    private LocalDateTime createAt; // 생성시점
+
+    public Bookmark(User user, ItemTrade itemTrade) {
+        this.user = user;
+        this.itemTrade = itemTrade;
+        this.createAt = LocalDateTime.now();
+    }
+}
