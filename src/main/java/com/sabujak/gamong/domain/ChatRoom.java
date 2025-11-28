4292be3284876d327613b8a65a9d04b237405cf1
aitchhhan <aitchhhan@gmail.com>
2025-11-28 16:10:15 +0900

    [add] ChatRoom domain

---
+++ src/main/java/com/sabujak/gamong/domain/ChatRoom.java
@@ -0,0 +1,30 @@
+package com.sabujak.gamong.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter // 재고 거래 채팅룸
+public class ChatRoom {
+    @Id
+    @GeneratedValue
+    private Long id; // pk
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_trade_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ItemTrade itemTrade; // 재고거래 글
+
+    private LocalDateTime createAt; // 생성 시점
+
+    public ChatRoom(ItemTrade itemTrade) {
+        this.itemTrade = itemTrade;
+        this.createAt = LocalDateTime.now();
+    }
+}
