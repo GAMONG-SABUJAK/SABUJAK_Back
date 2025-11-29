892db5b957050211bda1b77c3e2889d26404f8d1
aitchhhan <aitchhan@gmil.com>
2025-11-30 01:21:53 +0900

    [feat] makeChatRoom

--- src/main/java/com/sabujak/gamong/controller/ChatRoomController.java
+++ src/main/java/com/sabujak/gamong/controller/ChatRoomController.java
@@ -1,4 +1,24 @@
 package com.sabujak.gamong.controller;
 
+import com.sabujak.gamong.dto.Request.ReqItemTradeId;
+import com.sabujak.gamong.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chatroom")
 public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @PostMapping("/make")
+    public ResponseEntity<Void> makeChatRoom(ReqItemTradeId reqItemTradeId) {
+        chatRoomService.makeChatRoom(reqItemTradeId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
--- src/main/java/com/sabujak/gamong/service/ChatRoomService.java
+++ src/main/java/com/sabujak/gamong/service/ChatRoomService.java
@@ -1,4 +1,29 @@
 package com.sabujak.gamong.service;
 
+import com.sabujak.gamong.domain.ChatRoom;
+import com.sabujak.gamong.domain.ItemTrade;
+import com.sabujak.gamong.dto.Request.ReqItemTradeId;
+import com.sabujak.gamong.exception.EmptyItemTradeIdException;
+import com.sabujak.gamong.repository.ChatRoomRepository;
+import com.sabujak.gamong.repository.ItemTradeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final ItemTradeRepository itemTradeRepository;
+
+    @Transactional
+    public void makeChatRoom(ReqItemTradeId reqItemTradeId) {
+        ItemTrade itemTrade = itemTradeRepository.findById(reqItemTradeId.itemTradeId())
+                .orElseThrow(EmptyItemTradeIdException::new);
+
+        ChatRoom chatRoom = new ChatRoom(itemTrade);
+        chatRoomRepository.save(chatRoom);
+    }
 }
