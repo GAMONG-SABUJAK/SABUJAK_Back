7d092c8602b92333268b9a45f437ecc70e0765d0
aitchhhan <aitchhan@gmil.com>
2025-11-30 03:24:46 +0900

    [feat] getItemTrade

--- src/main/java/com/sabujak/gamong/controller/ItemTradeController.java
+++ src/main/java/com/sabujak/gamong/controller/ItemTradeController.java
@@ -2,6 +2,7 @@ package com.sabujak.gamong.controller;
 
 import com.sabujak.gamong.domain.User;
 import com.sabujak.gamong.dto.Request.ReqItemTrade;
+import com.sabujak.gamong.dto.Response.ItemTradeRes;
 import com.sabujak.gamong.service.ItemTradeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,5 +27,10 @@ public class ItemTradeController {
     public ResponseEntity<String> deleteItemTrade(@AuthenticationPrincipal User user, @PathVariable Long itemTradeId) {
         itemTradeService.deleteItemTrade(user, itemTradeId);
         return ResponseEntity.status(HttpStatus.OK).body("재고 거래 글 삭제 성공");
+    }
+
+    @GetMapping("/{itemTradeId}")
+    public ResponseEntity<ItemTradeRes> getItemTrade(@PathVariable Long itemTradeId) {
+        return ResponseEntity.status(HttpStatus.OK).body(itemTradeService.getItemTrade(itemTradeId));
     }
 }
--- src/main/java/com/sabujak/gamong/repository/ChatRoomRepository.java
+++ src/main/java/com/sabujak/gamong/repository/ChatRoomRepository.java
@@ -4,4 +4,5 @@ import com.sabujak.gamong.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    long countChatRoomByItemTradeId(Long itemTradeId);
 }
