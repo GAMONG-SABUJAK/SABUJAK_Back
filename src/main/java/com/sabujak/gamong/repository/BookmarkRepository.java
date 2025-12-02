9153931abd95a135172532d3212ab0f654fd1fd1
aitchhhan <aitchhhan@gmail.com>
2025-12-02 23:58:49 +0900

    [fix] Troubleshooting N+1

--- src/main/java/com/sabujak/gamong/repository/BookmarkRepository.java
+++ src/main/java/com/sabujak/gamong/repository/BookmarkRepository.java
@@ -3,7 +3,10 @@ package com.sabujak.gamong.repository;
 import com.sabujak.gamong.domain.Bookmark;
 import com.sabujak.gamong.domain.ItemTrade;
 import com.sabujak.gamong.domain.User;
+import com.sabujak.gamong.dto.Response.ItemTradeRes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +17,26 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     long countBookmarkByItemTradeId(Long itemTradeId);
 
     List<Bookmark> findByUser(User user);
+
+    @Query("""
+        SELECT new com.sabujak.gamong.dto.Response.ItemTradeRes(
+            i.id,
+            i.hashTag,
+            i.itemName,
+            i.title,
+            i.description,
+            i.price,
+            u.address,
+            COUNT(DISTINCT c.id),
+            COUNT(DISTINCT b2.id)
+        )
+        FROM Bookmark b
+        JOIN b.itemTrade i
+        JOIN i.user u
+        LEFT JOIN i.chatRoomList c
+        LEFT JOIN i.bookmarkList b2
+        WHERE b.user = :user
+        GROUP BY i.id, i.hashTag, i.itemName, i.title, i.description, i.price, u.businessAddress
+    """)
+    List<ItemTradeRes> findBookmarkedItemTradesByUser(@Param("user") User user);
 }
