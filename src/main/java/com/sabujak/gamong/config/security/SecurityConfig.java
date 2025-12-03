3a30f43f4ac263983fc7cbd4b6448582e6fb3e5b
aitchhhan <aitchhhan@gmail.com>
2025-12-03 20:52:04 +0900

    [feat] SecurityConfig

--- src/main/java/com/sabujak/gamong/config/security/SecurityConfig.java
+++ src/main/java/com/sabujak/gamong/config/security/SecurityConfig.java
@@ -1,29 +1,63 @@
 package com.sabujak.gamong.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final JwtAuthenticationEntryPoint authenticationEntryPoint;
-
-    public SecurityConfig(JwtAuthenticationEntryPoint authenticationEntryPoint) {
-        this.authenticationEntryPoint = authenticationEntryPoint;
-    }
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(withDefaults()) // cors 등록
+                .httpBasic(AbstractHttpConfigurer::disable) // Spring Security의 기본 인증 방식인 Basic Authentication을 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // JWT는 CSRF 공격에 취약하지 않아 CSRF 보호 비활성화 // 보통 CSRF 보호는 세션 기반 인증을 위해 사용
+                .formLogin(AbstractHttpConfigurer::disable) //  Spring Security의 기본 폼 로그인 기능을 비활성화
+                // JWT를 사용하기 때문에 세션을 사용하지 않도록 STATELESS로 설정
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                )
                 .exceptionHandling(e ->
                         e.authenticationEntryPoint(authenticationEntryPoint)
-                                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                                .accessDeniedHandler(accessDeniedHandler)
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://domain.com")); // 허용할 Origin(요청 출처) 지정
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 지정
+        config.setAllowedHeaders(List.of("*")); // 허용할 요청 헤더 지정 // "*"로 지정하면 모든 헤더를 허용함
+        config.setAllowCredentials(true); // true로 설정하면 클라이언트에서 쿠키 포함 요청이 가능
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // URL 패턴에 따라 위의 CORS 설정을 등록할 객체 생성
+        source.registerCorsConfiguration("/**", config); // 모든 엔드포인트에 대해 위에서 정의한 CORS 설정 적용
+        return source; // Spring Security에서 참조할 CORS 설정 소스 반환
     }
 }
 
